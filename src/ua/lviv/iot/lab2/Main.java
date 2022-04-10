172b330e31c8188f61e44320163255fac1ba209d
Ruslanexe <ruslangurskij5@gmail.com>
2022-04-10 23:43:57 +0300

    add 2 lab

--- src/ua/lviv/iot/lab2/Main.java
+++ src/ua/lviv/iot/lab2/Main.java
@@ -12,26 +12,6 @@ import java.util.List;
 public  class Main {
 
     public static void main(String[] args) {
-    hockey_stick Hockey_stick =new hockey_stick("BloodEagle",1200,3,typeOfHockeyEquipment.fotTheAttacker,"black");
-    hockey_armor Hockey_armor = new hockey_armor("StoneField",3000,12,typeOfHockeyEquipment.forTheReferee);
-    List<AbstractHockeyEquipment> list = new LinkedList<>();
-    list.add(Hockey_stick);
-    list.add(Hockey_armor);
-    hockeyClubManager manager = new hockeyClubManager(list);
-    System.out.println(manager.SortedByGrowth().get(0).getPrice());
-    System.out.println(manager.SortedByGrowth().get(1).getPrice());
-        System.out.println("///////////////////////////////////////////////");
 
-    System.out.println(manager.SortedByDescending().get(0).getPrice());
-    System.out.println(manager.SortedByDescending().get(1).getPrice());
-        System.out.println("///////////////////////////////////////////////");
-
-    System.out.println(manager.sortByGrowthAlphabet().get(0).getName());
-    System.out.println(manager.sortByGrowthAlphabet().get(1).getName());
-    System.out.println("///////////////////////////////////////////////");
-
-        System.out.println(manager.sortByDescendingAlphabet().get(0).getName());
-        System.out.println(manager.sortByDescendingAlphabet().get(1).getName());
-        System.out.println("///////////////////////////////////////////////");
     }
 }
--- src/ua/lviv/iot/lab2/manager/hockeyClubManager.java
+++ src/ua/lviv/iot/lab2/manager/hockeyClubManager.java
@@ -4,28 +4,4 @@ import ua.lviv.iot.lab2.manager.AbstractHockeyEquipment;
 import java.util.stream.Collectors;
 public class hockeyClubManager{
     private List<AbstractHockeyEquipment> list;
-
-    public hockeyClubManager(List<AbstractHockeyEquipment> list) {
-        this.list = list;
-    }
-    public List<AbstractHockeyEquipment>SortedByGrowth(){
-        List<AbstractHockeyEquipment> sortedList = this.list;
-        sortedList.sort((o1,o2) -> Double.compare(o1.getPrice(),o2.getPrice()));
-        return sortedList;
-    }
-    public List<AbstractHockeyEquipment>SortedByDescending(){
-        List<AbstractHockeyEquipment> sortedList = this.list;
-        sortedList.sort((o1,o2) -> -Double.compare(o1.getPrice(),o2.getPrice()));
-        return sortedList;
-}
-    public List<AbstractHockeyEquipment> sortByGrowthAlphabet(){
-        List<AbstractHockeyEquipment> sortedList = this.list;
-        sortedList.sort((o1,o2) -> (o1.getName().compareTo(o2.getName())));
-        return sortedList;
-    }
-    public List<AbstractHockeyEquipment> sortByDescendingAlphabet(){
-        List<AbstractHockeyEquipment> sortedList = this.list;
-        sortedList.sort(((o2, o1) -> -(o2.getName().compareTo(o1.getName()))));
-        return sortedList;
-    }
 }
